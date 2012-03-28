d1823d7870bc694a02b408756f2371a2357ab533
Uli Armbruster <uli.armbruster@heco.de>
2012-03-28 13:30:38 +0200

    Created Structure

---
+++ UAR.Persistence.ORM/AdventureWorksDbModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// Default code generation is disabled for model 'G:\Private\EntityFrameworkV2\UAR.Persistence.ORM\UAR.Persistence.ORM\AdventureWorksDbModel.edmx'. 
+// To enable default code generation, change the value of the 'Code Generation Strategy' designer
+// property to an alternate value. This property is available in the Properties Window when the model is
+// open in the designer.
---
+++ UAR.Persistence.ORM/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UAR.Persistence.ORM
+{
+    public class Class1
+    {
+    }
+}
